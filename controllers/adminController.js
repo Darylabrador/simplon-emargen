cd6421a4a6bf27df46fa903142f9a993ce9b6cb2
Darylabrador <abrador.daryl@gmail.com>
2020-08-03 21:23:12 +0400

    Add : gestion google sheet backend

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const axios                = require('axios');
 
 /** get admin dashboard
  * @name getDashboard
@@ -29,6 +30,10 @@ exports.getDashboard = async (req, res) => {
  * @throws Will throw an error if one error occursed
  */
 exports.postSignOffShettPdf = async (req, res) => {
+    const apprenants     = [];
+    const joursFormation = [];
+    const formateur      = [];
+
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -38,7 +43,30 @@ exports.postSignOffShettPdf = async (req, res) => {
         });
     }
 
+    const { createdBy, intitule, dataSheetUrl } = req.body;
+    const infoUrl = dataSheetUrl.split('/')[5];
+
     try {
+        const response = await axios.get(`https://spreadsheets.google.com/feeds/cells/${infoUrl}/1/public/full?alt=json`); 
+        const infoJson = response.data.feed.entry;
+
+        infoJson.forEach(data => {
+            if (data.gs$cell.col == 1 && data.gs$cell.row != 1){
+                apprenants.push(data.gs$cell.inputValue);
+            }
+
+            if (data.gs$cell.col != 1 && data.gs$cell.row == 1) {
+                joursFormation.push(data.gs$cell.inputValue);
+            }
+
+            if (data.gs$cell.col != 1 && data.gs$cell.row == 2){
+                formateur.push(data.gs$cell.inputValue);
+            }
+        });
+
+        console.log(apprenants);
+        console.log(joursFormation);
+        console.log(formateur);
 
     } catch (error) {
         res.json({
