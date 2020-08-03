bb15381bc11de2e8fe79b668d6da9cd8c50b82ec
Darylabrador <abrador.daryl@gmail.com>
2020-08-03 15:26:06 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+var createError  = require('http-errors');
+var express      = require('express');
+var path         = require('path');
+var cookieParser = require('cookie-parser');
+var logger       = require('morgan');
+
+const mongoose     = require('mongoose');
+const dotenv       = require('dotenv').config();
+const session      = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const flash        = require('connect-flash');
+const helmet       = require('helmet');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// Add robust session handler
+const store = new MongoDBStore({
+  uri: configDB.url,
+  collection: 'sessions'
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(helmet());
+app.disable('x-powered-by');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Simple session
+app.use(
+  session({
+    name: 'default',
+    secret: 'asq4b4PRJhpo025HjqeZaEasdz68D',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      sameSite: true,
+      maxAge: 3600 * 1000 * 3
+    }
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash('success');
+  res.locals.error_message   = req.flash('error');
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// general error handler (all except 404)
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.status(error.httpStatusCode).render('error', {
+    title: 'Une erreur est servenue',
+    path: '/errors',
+    statusCode: error.httpStatusCode
+  });
+});
+
+module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,11 @@
+let dbUrl;
+
+if (process.env.DB_URL) {
+    dbUrl = process.env.DB_URL
+} else {
+    dbUrl = 'mongodb://localhost:27017/simplonvote';
+}
+
+module.exports = {
+    url: dbUrl
+}
