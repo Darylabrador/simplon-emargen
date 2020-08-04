57bc8ed3c44281a1dbc14d4513c0dfd3e9820b2a
Darylabrador <abrador.daryl@gmail.com>
2020-08-04 22:38:48 +0400

    Fix : Generation du PDF en copié collé du format réel

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,7 @@ const { validationResult } = require('express-validator');
 const axios                = require('axios');
 
 
+
 /** get admin dashboard
  * @name getDashboard
  * @function
@@ -88,7 +89,7 @@ exports.postSignOffShettPdf = async (req, res) => {
         const signoffPath = path.join('data', 'pdf', signoffPDF);
 
         const doc = new PDFDocument({
-            size: 'legal',
+            size: 'A4',
             layout: 'landscape'
         });
 
@@ -98,13 +99,13 @@ exports.postSignOffShettPdf = async (req, res) => {
         doc.pipe(fs.createWriteStream(signoffPath));
         doc.pipe(res);
 
-        doc.image(imageUploaded, 80, 50, {width: 150});
-        doc.fontSize(16);
+        doc.image(imageUploaded, 60, 50, {width: 150});
+        doc.fontSize(14);
         doc
             .font('Helvetica-Bold')
-            .text('FEUILLE D\'EMARGEMENT -> PERIODE DE FORMATION', 250, 65);
+            .text('FEUILLE D\'EMARGEMENT -> PERIODE DE FORMATION', 230, 65);
 
-        doc.fontSize(9)
+        doc.fontSize(8)
 
         doc
             .font('Helvetica-Bold')
@@ -116,12 +117,90 @@ exports.postSignOffShettPdf = async (req, res) => {
 
         doc
             .font('Helvetica')
-            .text('SIMPLON REUNION', 400, 95);
+            .text('SIMPLON REUNION', 340, 92);
     
- 
+        let xEntete = 200;
+        let yEntete = 200;
+        
+        joursFormation.forEach(jour =>{
+            // La date
+            doc.lineJoin('miter')
+                .rect(xEntete, yEntete, 120, 22)
+                .stroke()
+                .font('Helvetica-Bold')
+                .text(`Le ${jour}`, xEntete + 30, yEntete + 8);
+                xEntete += 120; // pour décalage par rapport à la largeur
+
+        });
+
+        let xApprenant = 30;
+        let yApprenant = 222;
+
+        doc.lineJoin('miter')
+            .rect(xApprenant, yApprenant, 170, 30)
+            .stroke()
+            .font('Helvetica-Bold')
+            .text(`NOM PRENOM Apprenant`, 60, 234);
+
+    
+        // Notion matin / apres midi
+        for (let i = 0; i < 10; i++){
+            doc.fontSize(6);
+            if(i%2 == 0) {
+                doc.lineJoin('miter')
+                    .rect(200 + (60 * i), yApprenant, 60, 30)
+                    .stroke()
+                    .font('Helvetica-Bold')
+                    .text('MATIN', 205 + (60 * i), yApprenant + 8, {width: 60, align: 'left'})
+                    .text('Durée en h : 4', 205 + (60 * i), yApprenant + 16, {width: 60, align: 'left'});
+            }else {
+                doc.lineJoin('miter')
+                    .rect(200 + (60 * i), yApprenant, 60, 30)
+                    .stroke()
+                    .font('Helvetica-Bold')
+                    .text('APRES-MIDI', 205 + (60 * i), yApprenant + 8, {width: 60, align: 'left'})
+                    .text('Durée en h : 3', 205 + (60 * i), yApprenant + 16, {width: 60, align: 'left'});
+            }
+        }
+
+        apprenants.forEach(apprenant =>{
+            doc.fontSize(7);
+
+            // Les nom prenom des apprenants
+            doc.lineJoin('miter')
+                .rect(xApprenant, yApprenant + 30, 170, 40)
+                .stroke()
+                .font('Helvetica-Bold')
+                .text(`${apprenant}`, xApprenant + 8, yApprenant + 48);
+
+            // emplacement signature
+            for (let j = 0; j < 10; j++) {
+                doc.lineJoin('miter')
+                    .rect(xApprenant + (170 + 60 * j), yApprenant + 30, 60, 40)
+                    .stroke()
+            }
+            yApprenant += 40;
+        });
+
+        // nom prénom formateur
+        for(let k = 0; k < formateur.length; k++){
+            doc.fontSize(8);
+            doc.lineJoin('miter')
+                .rect(xApprenant + 170 + (120 * k), yApprenant + 30, 120, 60)
+                .stroke()
+                .font('Helvetica-Bold')
+                .text('NOM Prénom formateur.rice', xApprenant  + 170 + (120 * k), yApprenant + 38, {width: 120, align: 'center'})
+                .text(`${formateur[k]}`, xApprenant + 170 + (120 * k), yApprenant + 58, {width: 120, align: 'center'});
+        }
+
+        // Emplacement cachet de l'organisme de formation
+        doc.lineJoin('miter')
+            .rect(xApprenant + 550, yApprenant + 100, 220, 80)
+            .stroke()
+            .font('Helvetica')
+            .text(`Cachet organisme de formation :`, 585, yApprenant + 108);
 
         doc.end();
-
 
     } catch (error) {
         console.log(error);
