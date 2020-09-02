02e3100aa2bb2cb72c199cfb73a976735fdf2289
Darylabrador <abrador.daryl@gmail.com>
2020-09-02 18:47:12 +0400

    add : front login sans interaction

---
+++ www/js/login.js
@@ -0,0 +1,16 @@
+let errorMessage        = document.getElementById('error');
+let loginFormContainer  = document.getElementById('loginFormContainer');
+
+loginFormContainer.addEventListener('submit', evt => {
+    evt.preventDefault();
+    errorMessage.innerHTML = `
+        <div class="alert alert-danger alert-dismissible fade show my-0" role="alert">
+            <strong>Message de test </strong>
+        </div>
+    `;
+
+    setTimeout(() => {
+        location.href = 'accueil.html';
+    }, 2000);
+    
+})
