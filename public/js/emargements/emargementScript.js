f8455d9e380e02e076d3d1084a47f44722aa565f
Darylabrador <abrador.daryl@gmail.com>
2020-09-05 13:34:30 +0400

    Fix : Correction défaut du btn pour générer le QR code (eviter les interactions sur du span)

--- public/js/emargements/emargementScript.js
+++ public/js/emargements/emargementScript.js
@@ -1,25 +1,23 @@
-$(function() {
-    let btnSynchroInfo   = document.querySelectorAll('.btnSynchroInfo');
-    let emargementId     = document.getElementById("emargementId");
-    let emargementIdSign = document.getElementById("emargementIdSign");
-    let linkBtn          = document.querySelectorAll('.linkBtn');
+let btnSynchroInfo   = document.querySelectorAll('.btnSynchroInfo');
+let emargementId     = document.getElementById("emargementId");
+let emargementIdSign = document.getElementById("emargementIdSign");
+let linkBtn          = document.querySelectorAll('.linkBtn');
 
-    // handle click on synchro btn
-    if (btnSynchroInfo.length != 0) {
-        btnSynchroInfo.forEach(btn => {
-            btn.addEventListener("click", () => {
-                emargementId.value = btn.getAttribute("data-id");
-            })
+// handle click on synchro btn
+if (btnSynchroInfo.length != 0) {
+    btnSynchroInfo.forEach(btn => {
+        btn.addEventListener("click", () => {
+            emargementId.value = btn.getAttribute("data-id");
         })
-    }
+    })
+}
 
-    // handle click on calendar btn (generate qrcode to sign)
-    if (linkBtn.length != 0) {
-        for (let i = 0; i < linkBtn.length; i++) {
-            linkBtn[i].addEventListener("click", () => {
-                emargementIdSign.value = linkBtn[i].getAttribute("data-id")
-                $('#modalSignature').modal('toggle');
-            })
-        }
-    }
-})
+// handle click on calendar btn (generate qrcode to sign)
+if (linkBtn.length != 0) {
+    linkBtn.forEach(btnGenerate => {
+        btnGenerate.addEventListener('click', () => {
+            emargementIdSign.value = btnGenerate.getAttribute("data-id")
+            $('#modalSignature').modal('toggle');
+        })
+    })
+}
